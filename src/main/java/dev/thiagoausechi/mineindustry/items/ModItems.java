22cf3b35a4eafb4fb7284312daf735a3415e7366
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-07-20 05:16:18 -0300

    feat: Add Wrench Item

--- src/main/java/dev/thiagoausechi/mineindustry/items/ModItems.java
+++ src/main/java/dev/thiagoausechi/mineindustry/items/ModItems.java
@@ -5,9 +5,12 @@ import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, References.MOD_ID);
+
+    public static final RegistryObject<Item> WRENCH = ITEMS.register("wrench", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
