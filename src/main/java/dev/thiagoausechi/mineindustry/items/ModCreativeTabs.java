abab54118277253202c3df589c39050a9a3f7d13
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-07-20 05:13:24 -0300

    feat: Add the Mod's Creative Tab

--- src/main/java/dev/thiagoausechi/mineindustry/items/ModCreativeTabs.java
+++ src/main/java/dev/thiagoausechi/mineindustry/items/ModCreativeTabs.java
@@ -2,12 +2,22 @@ package dev.thiagoausechi.mineindustry.items;
 
 import dev.thiagoausechi.mineindustry.References;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModCreativeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, References.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> MINEINDUSTRY_TAB = CREATIVE_MODE_TABS.register("mineindustry",
+            () -> CreativeModeTab.builder()
+                    .icon(Items.REDSTONE::getDefaultInstance)
+                    .title(Component.translatable("creativetab.mineindustry_tab"))
+                    .displayItems((parameters, output) -> {
+                    }).build());
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
