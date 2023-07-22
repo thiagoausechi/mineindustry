f69e25ab1b06202162e3c72dd73b57ed8abc21ed
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-07-22 05:10:13 -0300

    feat: Adopt BlockState DataGen to work with Registry System

--- src/main/java/dev/thiagoausechi/mineindustry/blocks/registries/IRegistry.java
+++ src/main/java/dev/thiagoausechi/mineindustry/blocks/registries/IRegistry.java
@@ -1,9 +1,11 @@
 package dev.thiagoausechi.mineindustry.blocks.registries;
 
+import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.registries.RegistryObject;
 
 public interface IRegistry<T> {
 
     RegistryObject<T> register();
-    
+
+    void registerStatesAndModels(BlockStateProvider provider, RegistryObject<T> registryObject);
 }
--- src/main/java/dev/thiagoausechi/mineindustry/datagen/ModBlockStateProvider.java
+++ src/main/java/dev/thiagoausechi/mineindustry/datagen/ModBlockStateProvider.java
@@ -3,10 +3,8 @@ package dev.thiagoausechi.mineindustry.datagen;
 import dev.thiagoausechi.mineindustry.References;
 import dev.thiagoausechi.mineindustry.blocks.ModBlocks;
 import net.minecraft.data.PackOutput;
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlockStateProvider extends BlockStateProvider {
 
@@ -16,12 +14,7 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        ModBlocks.DATA_GEN.forEach(this::blockWithItem);
+        ModBlocks.BLOCKS_REGISTRIES.forEach((block, registry) -> registry.registerStatesAndModels(this, block));
     }
-
-    private void blockWithItem(RegistryObject<? extends Block> blockRegistryObject) {
-        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
-    }
-
 
 }
