d7c066fba4a1661a768c92721bf7c0b5b7657bbd
Thiago Ausechi <thiago_ausechi@icloud.com>
2023-07-22 05:13:11 -0300

    feat: Add DrillBlock class

---
+++ src/main/java/dev/thiagoausechi/mineindustry/blocks/DrillBlock.java
@@ -0,0 +1,9 @@
+package dev.thiagoausechi.mineindustry.blocks;
+
+import net.minecraft.world.level.block.Block;
+
+public class DrillBlock extends Block {
+    public DrillBlock(Properties properties) {
+        super(properties);
+    }
+}
